4c196516f036e4eaa66075c088cad06be5985c25
Daniel Brito <daniell-henrique@hotmail.com>
2025-01-28 21:25:47 -0300

    chore: bootstrap project

---
+++ src/main/kotlin/com/polymatus/financial_control_service/FinancialControlServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.polymatus.financial_control_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialControlServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancialControlServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/polymatus/financial_control_service/FinancialControlServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.polymatus.financial_control_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancialControlServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
