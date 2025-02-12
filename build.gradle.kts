3804e6567af80c94f4fc396be2ebbc7cbc1df14c
Daniel Brito <daniell-henrique@hotmail.com>
2025-02-11 23:26:45 -0300

    chore: add db dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.jpa") version "1.9.25"
 
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
@@ -10,8 +11,6 @@ plugins {
 	id("io.gitlab.arturbosch.detekt") version "1.23.7"
 	id("jacoco")
 	id("org.sonarqube") version "5.1.0.4882"
-
-	id("org.flywaydb.flyway") version "11.3.1"
 }
 
 group = "com.polymatus"
@@ -30,17 +29,23 @@ repositories {
 val assertJVersion = "3.26.3"
 val detektVersion = "1.23.7"
 val mockkVersion = "1.13.12"
+val jacksonVersion = "2.18.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+	implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 
 	compileOnly("org.springframework.boot:spring-boot-devtools")
+
+	runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.assertj:assertj-core:$assertJVersion")
