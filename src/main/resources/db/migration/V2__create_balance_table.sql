8b36d631c63f136904f073b20d17a188b9215a0b
Daniel Brito <daniell-henrique@hotmail.com>
2025-02-11 23:37:58 -0300

    chore: add migration for balance table

---
+++ src/main/resources/db/migration/V2__create_balance_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE balance(
+    id SERIAL NOT NULL,
+    source VARCHAR(50) NOT NULL,
+    amount DECIMAL NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
