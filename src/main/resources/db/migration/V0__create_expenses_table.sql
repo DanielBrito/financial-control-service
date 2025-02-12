d6e9b3dd22602deb8e08ad1cf7e483f42dd7b3b9
Daniel Brito <daniell-henrique@hotmail.com>
2025-02-11 23:27:11 -0300

    chore: add migration for expenses table

---
+++ src/main/resources/db/migration/V0__create_expenses_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE expenses(
+    id SERIAL PRIMARY KEY,
+    priority INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price DECIMAL NOT NULL,
+    description VARCHAR(255),
+    place VARCHAR(50),
+    url VARCHAR,
+    comment VARCHAR,
+    "group" VARCHAR(50)
+);
