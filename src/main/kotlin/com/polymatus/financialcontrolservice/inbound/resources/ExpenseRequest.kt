3ca8861e04e92dff3ea3815880f675a2a1e775c0
Daniel Brito <daniell-henrique@hotmail.com>
2025-05-08 23:12:34 -0300

    feat: add field validation to dto

--- src/main/kotlin/com/polymatus/financialcontrolservice/inbound/resources/ExpenseRequest.kt
+++ src/main/kotlin/com/polymatus/financialcontrolservice/inbound/resources/ExpenseRequest.kt
@@ -1,13 +1,34 @@
 package com.polymatus.financialcontrolservice.inbound.resources
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
+import org.hibernate.validator.constraints.Range
+import org.hibernate.validator.constraints.URL
+
 data class ExpenseRequest(
+    @field:NotNull(message = "Priority is required.")
+    @field:Range(min = 1, max = 4, "Priority must be between 1 (min) and 4 (max).")
     val priority: Int,
+
+    @field:NotBlank(message = "Name is required.")
     val name: String,
+
+    @field:NotBlank(message = "Category is required.")
     val category: String,
+
+    @field:NotNull(message = "Price is required.")
+    @field:Positive("Price must be greater than R$ 0.00")
     val price: Double,
-    val description: String,
+
+    val description: String?,
     val place: String?,
+
+    @field:URL(message = "URL must be valid.")
     val url: String?,
+
     val comment: String?,
+
+    @field:NotBlank(message = "Group is required.")
     val group: String
 )
