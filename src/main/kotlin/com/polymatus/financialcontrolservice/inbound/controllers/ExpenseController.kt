0e3f5b75542bdbbf5b7c26f9740db9adb9e5a993
Daniel Brito <daniell-henrique@hotmail.com>
2025-05-08 23:26:37 -0300

    feat: add controller to manage expenses

---
+++ src/main/kotlin/com/polymatus/financialcontrolservice/inbound/controllers/ExpenseController.kt
@@ -0,0 +1,21 @@
+package com.polymatus.financialcontrolservice.inbound.controllers
+
+import com.polymatus.financialcontrolservice.inbound.controllers.resources.ExpenseRequest
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ExpenseController {
+
+    @PostMapping("/expenses")
+    fun createExpense(
+        @Valid @RequestBody expenseRequest: ExpenseRequest
+    ): ResponseEntity<Any> {
+        println("Received request to create expense: $expenseRequest")
+        return ResponseEntity(CREATED)
+    }
+}
