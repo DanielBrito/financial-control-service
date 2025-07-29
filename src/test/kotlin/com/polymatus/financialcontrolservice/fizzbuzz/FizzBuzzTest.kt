9d9069c177ef7ad1d2423197a6a81ff87778ab5b
Daniel Brito <daniell-henrique@hotmail.com>
2025-07-29 19:27:05 -0300

    refactor: apply bdd in unit tests

--- src/test/kotlin/com/polymatus/financialcontrolservice/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/com/polymatus/financialcontrolservice/fizzbuzz/FizzBuzzTest.kt
@@ -1,67 +1,76 @@
 package com.polymatus.financialcontrolservice.fizzbuzz
 
+import io.kotest.core.spec.style.BehaviorSpec
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
 
-internal class FizzBuzzTest {
+internal class FizzBuzzTest : BehaviorSpec() {
 
     private val fizzBuzz = FizzBuzz()
 
-    @Nested
-    @DisplayName("given an integer n")
-    inner class GivenAnIntegerN {
-
-        @Nested
-        @DisplayName("when iterating from 1 to n")
-        inner class WhenIteratingFrom1ToN {
-
-            @Test
-            fun `adds FizzBuzz to result when i is divisible by 3 and 5`() {
+    init {
+        context("fizz buzz problem solution") {
+            given("an integer n divisible by 3 and 5") {
                 val n = 15
 
-                val result = fizzBuzz.solve(n)
+                `when`("iterating i from 1 to n") {
+                    val result = fizzBuzz.solve(n)
 
-                assertThat(result).contains("FizzBuzz")
+                    then("adds FizzBuzz to result") {
+                        assertThat(result).contains("FizzBuzz")
+                    }
+                }
             }
 
-            @Test
-            fun `adds Fizz to result when i is divisible by 3`() {
+            given("an integer n divisible by 3") {
                 val n = 3
 
-                val result = fizzBuzz.solve(n)
+                `when`("iterating i from 1 to n") {
+                    val result = fizzBuzz.solve(n)
 
-                assertThat(result).contains("Fizz")
+                    then("adds Fizz to result") {
+                        assertThat(result).contains("Fizz")
+                    }
+                }
             }
 
-            @Test
-            fun `adds Buzz to result when i is divisible by 5`() {
+            given("an integer n divisible by 5") {
                 val n = 5
 
-                val result = fizzBuzz.solve(n)
+                `when`("iterating i from 1 to n") {
+                    val result = fizzBuzz.solve(n)
 
-                assertThat(result).contains("Buzz")
+                    then("adds Buzz to result") {
+                        assertThat(result).contains("Buzz")
+                    }
+                }
             }
 
-            @Test
-            fun `adds i to result when i is not divisible by 3 and 5`() {
+            given("an integer n not divisible by 3 nor 5") {
                 val n = 2
-                val expected = listOf("1", "2")
 
-                val result = fizzBuzz.solve(n)
+                `when`("iterating i from 1 to n") {
+                    val result = fizzBuzz.solve(n)
 
-                assertThat(result).isEqualTo(expected)
+                    then("adds i to result") {
+                        val expected = listOf("1", "2")
+
+                        assertThat(result).isEqualTo(expected)
+                    }
+                }
             }
 
-            @Test
-            fun `returns only one element when n is 1`() {
+            given("an integer n equal to 1") {
                 val n = 1
-                val expected = listOf("1")
 
-                val result = fizzBuzz.solve(n)
+                `when`("iterating i from 1 to n") {
+                    val result = fizzBuzz.solve(n)
 
-                assertThat(result).isEqualTo(expected)
+                    then("adds only one element") {
+                        val expected = listOf("1")
+
+                        assertThat(result).isEqualTo(expected)
+                    }
+                }
             }
         }
     }
