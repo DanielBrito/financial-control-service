8c639c0f776206ea70f3cd1695e771cfef933b75
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-14 10:23:25 -0300

    refactor: remove unused args

--- src/main/kotlin/com/polymatus/financialcontrolservice/FinancialControlServiceApplication.kt
+++ src/main/kotlin/com/polymatus/financialcontrolservice/FinancialControlServiceApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class FinancialControlServiceApplication
 
-fun main(args: Array<String>) {
-    runApplication<FinancialControlServiceApplication>{}
+fun main() {
+    runApplication<FinancialControlServiceApplication> {}
 }
